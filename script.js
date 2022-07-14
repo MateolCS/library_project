1b12e80891acc7ccff0843a3a6ad71ddd1dc77d5
MateoLCS <mateusz.namysl0318@gmail.com>
2022-07-14 17:47:24 +0200

    Book class and Library class

--- script.js
+++ script.js
@@ -1,158 +1,64 @@
-const library =[]
-//Buttons and areas that we will be working with
 
-let addButton = document.querySelector('.open-form')
-let submitButton = document.querySelector('.submit-form')
-let statusButton = document.querySelector('.status-button')
-let deleteCard = document.querySelector('.delete-card')
-let form = document.querySelector('form')
-let mainDisplay = document.querySelector('.cards')
+const addButton = document.querySelector('.open-form')
+const submitButton = document.querySelector('.submit-form')
+const statusButton = document.querySelector('.status-button')
+const deleteCard = document.querySelector('.delete-card')
+const form = document.querySelector('form')
+const mainDisplay = document.querySelector('.cards')
 
 
+class Library {
+    books = []
 
-// creating book class
+    constructor(inBooks){
+        if(inBooks === undefined){
+            this.books = []
+        }else{
+            this.books = inBooks    
+        }
+         
+    }
+
+    addBook(inBook){
+        this.books.push(inBook)
+    }
+
+    deleteBook(bookName){
+        this.books = this.books.filter((book) => book.getName() !== bookName)
+    }
+
+    getBooks(){
+        return this.books
+    }
+}
 
 class Book{
-    constructor(title, author, pages, status,){
-        this.title = title
+    constructor(name, author, pages, status){
+        this.name = name
         this.author = author
         this.pages = pages
         this.status = status
-        this.createCard = function(){
-            let card = document.createElement('div')
-            card.classList.add('card')
-            let deleteButton = document.createElement('button')
-            deleteButton.classList.add('delete-card')
-            deleteButton.innerHTML = "&times;"
-            deleteButton.addEventListener('click', ()=>{
-                card.remove()
-                removeFromLibrary(library, title)
-                console.log(library)
-            })
-            card.appendChild(deleteButton)
-            let cardTitle = document.createElement('h3')
-            cardTitle.innerText = `${title}`
-            cardTitle.classList.add('desc')
-            cardTitle.classList.add('card-title')
-            card.appendChild(cardTitle)
-            let cardAuthor = document.createElement('h3')
-            cardAuthor.innerText = `${author}`
-            cardAuthor.classList.add('desc')
-            card.appendChild(cardAuthor)
-            let cardPages = document.createElement('h3')
-            cardPages.innerText = `${pages}`
-            cardPages.classList.add('desc')
-            card.appendChild(cardPages)
-            let cardStatus = document.createElement('button')
-            cardStatus.classList.add('status-button')
-            if(status == 'Done!'){
-                cardStatus.style.backgroundColor = '#A8B090'
-                cardStatus.style.border = '2px solid #99A083'
-                cardStatus.innerText = `${status}`
-            }else{
-                cardStatus.style.backgroundColor = '#dc2f02'
-                cardStatus.style.border = '2px solid #d00000'
-                cardStatus.innerText = `${status}`
-            }
-            card.appendChild(cardStatus)
-            cardStatus.addEventListener('click', () =>{
-                if(status == 'Done!'){
-                    cardStatus.style.backgroundColor = '#dc2f02'
-                    cardStatus.style.border = '2px solid #d00000'
-                    status = 'In progress!'
-                    cardStatus.innerText = `${status}`
-                }else{
-                    cardStatus.style.backgroundColor = '#A8B090'
-                    cardStatus.style.border = '2px solid #99A083'
-                    status = 'Done!'
-                    cardStatus.innerText = `${status}`
-                }
-            })
-            mainDisplay.appendChild(card)
-    }
-}}
-
-
-let book1 = new Book('jan', 'kowalk', 123, 'Done!')
-let book2 = new Book('adam', 'kowalk', 123, 'In progress!')
-let book3 = new Book('stanislaw', 'kowalk', 123, 'In progress!')
-let book4 = new Book('ania', 'kowalk', 123, 'In progress!')
-
-
-library.push(book1)
-library.push(book2)
-library.push(book3)
-library.push(book4)
-console.log(library)
-
-
-function removeFromLibrary(library, bookTitle){
-    for(let i = 0; library.length; i++){
-        if(library[i].title == bookTitle){
-            library.splice(i,1)
-            break
-        }
-    }
-}
-
-
-function fillPage(library){
-    for(let i = 0; i < library.length; i++){
-        library[i].createCard()
-    }
-}
-
-
-fillPage(library)
-
-
-
-const getBook = () =>{
-    let inputs = document.querySelector('.inputs')
-    let statusInfo = document.getElementById('status')
-    let status = ''
-
-    let title = inputs.children[0].value
-    let author = inputs.children[1].value
-    let pages = inputs.children[2].value
-
-    if(statusInfo.checked){
-        status = 'Done!'
-    }else{
-         status = 'In progress'
     }
 
-    return new Book(title, author, pages, status)
-}
-
-addButton.addEventListener('click', () =>{
-    form.style.visibility = 'visible'
-})
-
-submitButton.addEventListener('click', ()=>{
-    updateLibrary()
-})
-
-
-
-const updateLibrary = ()=>{
-    //let bookToAdd = getBook()
-
-    if(localStorage.getItem('library') == null){
-        localStorage.setItem('library', '[]')
+    getName(){
+        return this.name
     }
 
-    let oldLibrary = JSON.parse(localStorage.getItem('library'))
-    oldLibrary.push(getBook())
+    getAuthor(){
+        return this.author
+    }
 
-    localStorage.setItem('library', oldLibrary)
+    getPages(){
+        return this.pages
+    }
+
+    getStatus(){
+        return this.status
+    }
+
+    setStatus(){
+        this.status = !this.status
+    } 
 }
-
-const viewLibrary = ()=>{
-    var libraryToView = JSON.parse(localStorage.getItem('library'))
-    return libraryToView
-}
-
-
 
 
